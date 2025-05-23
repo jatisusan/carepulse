bc7ba5a745a258d8f30a9988afc82eebbf8a50ac
jatisusan <susurrus112@gmail.com>
2025-05-23 16:12:02 +0545

    admin page fixed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+
+
 import StatCard from "@/components/StatCard";
 import {columns} from "@/components/table/columns";
 import {DataTable} from "@/components/table/DataTable";
